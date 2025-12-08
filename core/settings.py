fc903a5a1b3c48cfe117fa8dc79519b91a4cf1d5
pavelixo <pavelixo@icloud.com>
2025-12-08 18:37:29 -0300

    Initializing storages

--- core/settings.py
+++ core/settings.py
@@ -83,3 +83,14 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = "static/"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = "minioadmin"
+AWS_SECRET_ACCESS_KEY = "minioadmin"
+AWS_STORAGE_BUCKET_NAME = "meu-bucket"
+AWS_S3_ENDPOINT_URL = "http://localhost:9000"
+AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
