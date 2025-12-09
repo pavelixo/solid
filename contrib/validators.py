73db3627542289d05975ce5cbdfe1da19831418f
pavelixo <pavelixo@icloud.com>
2025-12-08 21:37:03 -0300

    Creating FileUpload to rename files

--- contrib/validators.py
+++ contrib/validators.py
@@ -1,3 +1,6 @@
+import re
+from secrets import token_urlsafe
+
 from django.core import validators
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
@@ -23,3 +26,19 @@ class UsernameValidator(validators.RegexValidator):
         "- . (dot), _ (underscore), and - (hyphen)."
     )
     flags = 0
+
+
+@deconstructible
+class FileUpload:
+    EXT_REGEX = re.compile(r"\.([A-Za-z0-9]+)$")
+
+    def __call__(self, instance, filename):
+        """
+        Generates the new filename using a random string and file extension.
+        """
+
+        match = self.EXT_REGEX.search(filename)
+        ext = match.group(1) if match else ""
+
+        mark = token_urlsafe(16).lower()
+        return f"{mark}.{ext}" if ext else mark
