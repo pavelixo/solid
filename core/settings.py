5855252849f0ec4b0729b7ca8287724397f91f64
pavelixo <pavelixo@icloud.com>
2025-12-09 19:57:16 -0300

    Rename objects to storage

--- core/settings.py
+++ core/settings.py
@@ -15,9 +15,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "storages",
+    "storages", # django-storages
     "authentication",
-    "objects",
+    "storage",
 ]
 
 MIDDLEWARE = [
