fdaa11b24c7008766caf6a4cfe95564b9af5db7f
pavelixo <pavelixo@icloud.com>
2025-12-09 19:56:39 -0300

    Creating default models Folder, File

--- objects/models.py
+++ objects/models.py
@@ -1,3 +1,40 @@
+from secrets import token_urlsafe
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+def hash():
+    return token_urlsafe(12)
+
+class Folder(models.Model):
+    id = models.CharField(primary_key=True, max_length=32, default=hash, editable=False)
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="subfolders",
+        on_delete=models.CASCADE
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class File(models.Model):
+    id = models.CharField(primary_key=True, max_length=32, default=hash, editable=False)
+
+    folder = models.ForeignKey(
+        Folder,
+        related_name="files",
+        null=True, blank=True,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=255)
+    key = models.CharField(max_length=512)
+    size = models.BigIntegerField()
+    content_type = models.CharField(max_length=100)
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
