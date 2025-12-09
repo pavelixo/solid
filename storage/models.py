67df5fc738a73607d6936d7a3cf87d3dbbb79231
pavelixo <pavelixo@icloud.com>
2025-12-09 20:01:30 -0300

    Adding gettext

--- storage/models.py
+++ storage/models.py
@@ -1,6 +1,7 @@
 from secrets import token_urlsafe
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from authentication.models import User
 
@@ -11,8 +12,9 @@ def hash():
 
 class Folder(models.Model):
     id = models.CharField(primary_key=True, max_length=32, default=hash, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(_("name"), max_length=255)
     parent = models.ForeignKey(
+        _("parent"),
         "self",
         null=True,
         blank=True,
@@ -20,22 +22,33 @@ class Folder(models.Model):
         on_delete=models.CASCADE,
     )
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(_("owner"), User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("folder")
+        verbose_name_plural = _("folders")
+        abstract = True
 
 
 class File(models.Model):
     id = models.CharField(primary_key=True, max_length=32, default=hash, editable=False)
 
     folder = models.ForeignKey(
-        Folder, related_name="files", null=True, blank=True, on_delete=models.CASCADE
+        _("folder"),
+        Folder,
+        related_name="files",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
     )
 
-    name = models.CharField(max_length=255)
-    key = models.CharField(max_length=512)
-    size = models.BigIntegerField()
-    content_type = models.CharField(max_length=100)
+    name = models.CharField(_("name"), max_length=255)
+    key = models.CharField(_("key"), max_length=512)
+    size = models.BigIntegerField(_("size"))
+    content_type = models.CharField(_("content type"), max_length=100)
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(_("owner"), User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
