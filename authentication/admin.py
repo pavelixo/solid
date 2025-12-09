022dd4963b3c34bf9a6d429523a46f020ea9a2e9
pavelixo <pavelixo@icloud.com>
2025-12-08 21:46:38 -0300

    Creating User admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+    list_display = ("username", "display_username", "email", "is_staff", "is_superuser")
+
+    fieldsets = (
+        (
+            None,
+            {"fields": ("avatar", "username", "display_username", "email", "password")},
+        ),
+        ("Permissions", {"fields": ("is_superuser", "is_staff")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, UserAdmin)
