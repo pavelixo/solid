202b045a7cb3a473f88cf8d67dc6254a6eb3cc15
pavelixo <pavelixo@icloud.com>
2025-12-19 11:08:25 -0300

    Implementing file storage models and upload tasks

---
+++ storage/tests/test_models.py
@@ -0,0 +1,29 @@
+import pytest
+from authentication.models import User
+from storage.models import Folder, File
+
+
+@pytest.mark.django_db
+class TestModelStr:
+
+    @pytest.fixture
+    def user(self):
+        return User.objects.create_user(username="owner", email="owner@test.com")
+
+    def test_folder_str(self, user):
+        folder = Folder.objects.create(name="My Folder", owner=user)
+        assert str(folder) == "My Folder"
+
+    def test_file_str(self, user):
+        folder = Folder.objects.create(name="Photos", owner=user)
+        
+        file = File.objects.create(
+            name="vacation.jpg",
+            folder=folder,
+            owner=user,
+            key="path/to/vacation.jpg",
+            size=1024,
+            content_type="image/jpeg"
+        )
+        
+        assert str(file) == "vacation.jpg"
